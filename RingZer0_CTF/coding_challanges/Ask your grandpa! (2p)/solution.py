c3612944111346bfc2792f165e1fa81a5f01e72c
bu19akov <vovabulgakov00@mail.ru>
2024-01-21 16:36:45 +0100

    Add python code for automatic punch card decryption for Ask your grandpa!

--- RingZer0_CTF/coding_challanges/Ask your grandpa! (2p)/solution.py
+++ RingZer0_CTF/coding_challanges/Ask your grandpa! (2p)/solution.py
@@ -1,25 +1,3 @@
-# Ask your grandpa!
-
-## Challenge Details 
-
-- **CTF:** RingZer0
-- **Category:** Coding Challenges
-- **Points:** 2
-
-## Provided Materials
-
-- Image:
-
-![Image](./file.jpg)
-
-
-## Solution
-
-The name of the chellenge gives us a hint, that it is something `old` and indeed this is a [punch card](https://www.computerhope.com/jargon/p/punccard.htm). So with the help of [this](https://craftofcoding.wordpress.com/2017/01/28/read-your-own-punch-cards/) article we can manually decode the `punch card`.
-
-Also, we can write `python` program to do it automatically:
-
-```python
 from PIL import Image
 
 # Constants defining the dimensions and positions for decoding
@@ -98,10 +76,3 @@ def decode_card(image_path):
 # Usage
 decoded_data = decode_card("file.jpg")
 print(decoded_data)
-```
-
-## Final Flag
-
-`FLAG-B493801CDB-831ABDDFA628AC31`
-
-*Created by [bu19akov](https://github.com/bu19akov)*
